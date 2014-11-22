2e7b08fd7f925cc804c5e72d2425cb973cda9683
Yoshua Wuyts <yoshuawuyts@gmail.com>
2014-11-22 15:39:43 +0100

    index: update

--- src/index.js
+++ src/index.js
@@ -11,12 +11,83 @@ var exec = require('');
 module.exports = cmd;
 
 /**
+ * Prototype
+ */
+
+var cmd = Cmd.prototype;
+
+/**
  * Run a given tool with arguments.
  *
  * @param {String} name
  * @param {String[]?} args
  */
 
-function cmd(name, args) {
+function Cmd(name, args) {
+  if (!(this instanceof Cmd)) return new Cmd(name, args);
+  return this;
+}
 
+/**
+ * `cmd.clean()`
+ */
+
+cmd.clean = function() {
+  console.log('sorry, not implemented yet');
+}
+
+
+/**
+ * `cmd.doc()`
+ */
+
+cmd.doc = function() {
+  console.log('sorry, not implemented yet');
+}
+/**
+ * `cmd.fmt()`
+ */
+
+cmd.fmt = function() {
+  console.log('sorry, not implemented yet');
+}
+
+/**
+ * `cmd.init()`
+ */
+
+cmd.init = function() {
+  console.log('sorry, not implemented yet');
+}
+
+/**
+ * `cmd.lint()`
+ */
+
+cmd.lint = function() {
+  console.log('sorry, not implemented yet');
+}
+
+/**
+ * `cmd.list()`
+ */
+
+cmd.list = function() {
+  console.log('sorry, not implemented yet');
+}
+
+/**
+ * `cmd.test()`
+ */
+
+cmd.test = function() {
+  console.log('sorry, not implemented yet');
+}
+
+/**
+ * `cmd.version()`
+ */
+
+cmd.version = function() {
+  console.log('sorry, not implemented yet');
 }
