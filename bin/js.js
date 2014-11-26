39e9199c2d625ba662543f35ae18838840b61758
yoshuawuyts <i@yoshuawuyts.com>
2014-11-26 09:03:08 +0100

    lib: update

--- bin/js.js
+++ bin/js.js
@@ -7,34 +7,57 @@ var path = require('path');
 var fs = require('fs');
 var js = require('..');
 
+var cmd = js();
+
 /**
- * Process arguments.
+ * Parse cli args.
  */
 
 var args = process.argv.slice(2);
 var argv = minimist(args, {
-    alias: {
-      c: 'clean',
-      d: ['doc', 'docs'],
-      h: 'help',
-      l: 'lint',
-      ls: 'list',
-      t: 'test',
-      v: 'version'
-    },
-    boolean: [ 'h' ],
-    string: [ '_' ]
+  alias: {
+    h: 'help',
+    v: 'version'
+  },
+  boolean: ['h'],
+  string: ['_']
 });
 
 /**
- * Print usage.
+ * Flags.
  */
 
-if (argv.h || argv.help) return usage();
+if (argv._h || argv._help) return usage();
+if (argv.v || argv.version) return cmd.version();
 
 /**
- * `Usage`.
- * 
+ * Commands.
+ */
+
+var args = concat(argv._.splice(1));
+
+switch(argv._[0]) {
+  case 'clean':
+    return cmd.clean(args);
+  case 'doc':
+    return cmd.doc(args);
+  case 'fmt':
+    return cmd.fmt(args);
+  case 'init':
+    return cmd.init(args);
+  case 'lint':
+    return cmd.lint(args);
+  case 'list':
+    return cmd.list(args);
+  case 'test':
+    return cmd.test(args);
+  default:
+    return usage();
+}
+
+/**
+ * Print usage.
+ *
  * @param {Number} code
  * @api private
  */
@@ -43,7 +66,26 @@ function usage(code) {
   var rs = fs.createReadStream(path.join(__dirname, 'usage.txt'));
   rs.pipe(process.stdout);
 
-  if (code) rs.on('end', function() { 
+  if (code) rs.on('end', function() {
     process.exit(code);
   });
+}
+
+/**
+ * Concat arguments to a string.
+ *
+ * @param {Mixed}
+ * @return {String[]}
+ * @api private
+ */
+
+function concat() {
+  var arr = ' ';
+
+  for(i = 0, j = arguments.length; j > i; i++) {
+    arr += (arguments[i] + ' ');
+  }
+
+  console.log(arr)
+  return arr;
 }
